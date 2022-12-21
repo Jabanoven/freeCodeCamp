f2647f2143dd236d8997cb47497bcf81750df3dd
Jabanoven <tomasguakamole@yandex.ru>
2022-12-21 21:51:24 +0300

    Create freeCodeCamp.js

---
+++ freeCodeCamp.js
@@ -0,0 +1,25 @@
+/* ------ FREECODECAMP ------ */
+// Setup
+const lastName = "Lovelace";
+
+// Only change code below this line
+const lastLetterOfLastName = lastName[lastName.length - 1]; // Change this line
+
+/* ---------- */
+// Setup
+const lastName = "Lovelace";
+
+// Only change code below this line
+const secondToLastLetterOfLastName = lastName[lastName.length - 2]; // Change this line
+
+/* ---------- */
+const myNoun = "dog";
+const myAdjective = "big";
+const myVerb = "ran";
+const myAdverb = "quickly";
+
+// Only change code below this line
+const wordBlanks = myNoun + ' ' + myAdjective + ' ' + myVerb + ' ' + myAdverb; // Change this line
+// Only change code above this line
+
+/* ---------- */
